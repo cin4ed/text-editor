5046f917dd15e5134362ccfe304787afa9901bd2
Kenneth Quintero <kennethgqv@gmail.com>
2021-08-31 11:46:29 -0600

    Fix typo

--- kilo.c
+++ kilo.c
@@ -22,7 +22,7 @@ void enableRawMode() {
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
-  raw.c_cflag &= (CS8);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
